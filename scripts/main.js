621df03a31f12ba564d3f16ba1d597feb25d2074
mdaizal <mohdaizal@Metropolis.local>
2018-06-10 10:58:29 +0800

    Initialize

---
+++ scripts/main.js
@@ -0,0 +1,47 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello World!';
+
+var myImage = document.querySelector('img');
+myImage.onclick = () => {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/y15zr-kuning.jpg') {
+        myImage.setAttribute('src', 'images/y15zr-kuning-alt.jpg');
+    } else {
+        myImage.setAttribute('src', 'images/y15zr-kuning.jpg');
+    }
+}
+
+setUserName = () => { // latest style. old: function setUserName() {}
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Yamaha is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) { // first load page if no name stored in local storage will popup the prompt box to enter name
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Suzuki is cool too,' + storedName; // if already has name when reload still retain name but change the latter words (just for testing)
+}
+
+var myButton = document.getElementById('change');
+
+myButton.onclick = () => {
+    setUserName();
+}
+
+addNewMotorItem = (e) => {
+    var uList = document.getElementById('motorItem');
+    var listItem = document.createElement('li');
+    var newItem = prompt('Add new item:');
+    if (newItem != null) {
+        listItem.textContent = newItem;
+        uList.appendChild(listItem);
+    }
+}
+
+var motorButton = document.getElementById('newItem');
+
+motorButton.onclick = () => {
+    addNewMotorItem();
+}
